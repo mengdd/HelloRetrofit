e50cbbfab038d84358c68fe3a55051f4a891a056
Dandan MENG <mengdandanno1@163.com>
2016-11-02 17:12:27 +0800

    add bottom navigation view

--- app/src/main/java/com/ddmeng/helloretrofit/MainActivity.java
+++ app/src/main/java/com/ddmeng/helloretrofit/MainActivity.java
@@ -1,7 +1,10 @@
 package com.ddmeng.helloretrofit;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.ddmeng.helloretrofit.data.models.Endpoints;
@@ -9,6 +12,7 @@ import com.ddmeng.helloretrofit.data.remote.GitHubService;
 import com.ddmeng.helloretrofit.data.remote.ServiceGenerator;
 import com.ddmeng.helloretrofit.utils.LogUtils;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import retrofit2.Call;
@@ -17,11 +21,32 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
+    @BindView(R.id.bottom_navigation_view)
+    BottomNavigationView bottomNavigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        initView();
+    }
+
+    private void initView() {
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_user:
+                        return true;
+                    case R.id.action_repo:
+                        return true;
+                    case R.id.action_people:
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 
     @OnClick(R.id.get_all_endpoints)
